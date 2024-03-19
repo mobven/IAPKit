685f8cda0c835fc544180048fd8ea82bcfb3a772
Eser Kucuker <eser.kucuker@mobven.com>
2024-03-19 18:51:01 +0300

    add IAPPeriodUnit struct

---
+++ Sources/IAPKit/IAPProduct/IAPPeriodUnit.swift
@@ -0,0 +1,17 @@
+//
+//  IAPPeriodUnit.swift
+//
+//
+//  Created by Eser Kucuker on 19.03.2024.
+//
+
+import Foundation
+
+public struct IAPPeriodUnit {
+    public enum PeriodUnit: UInt{
+        case day = 0
+        case week = 1
+        case month = 2
+        case year = 3
+    }
+}

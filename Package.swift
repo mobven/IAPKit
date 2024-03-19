e03e21f03ee9fbf087957835ff798ea37b14aa57
Rashid Ramazanov <rasid.ramazanov@mobven.com>
2024-03-19 14:53:29 +0300

    Add platform iOS 12.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "IAPKit",
+    platforms: [.iOS(.v12)],
     products: [
         .library(
             name: "IAPKit",
@@ -12,7 +13,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/adaptyteam/AdaptySDK-iOS", from: "2.9.3"),
+        .package(url: "https://github.com/adaptyteam/AdaptySDK-iOS", exact: "2.9.3"),
         .package(url: "https://github.com/ReactiveX/RxSwift", from: "6.6.0")
     ],
     targets: [

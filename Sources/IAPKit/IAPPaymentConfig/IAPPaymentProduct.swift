62837f25881dcaf7ccfea14b6f6903da0a17feff
Eser Kucuker <eser.kucuker@mobven.com>
2024-05-05 12:34:35 +0300

    add IAPPaymentProduct

---
+++ Sources/IAPKit/IAPPaymentConfig/IAPPaymentProduct.swift
@@ -0,0 +1,30 @@
+//
+//  IAPPaymentProduct.swift
+//
+//
+//  Created by Eser Kucuker on 18.04.2024.
+//
+
+import Foundation
+import StoreKit
+
+public extension IAPPaymentConfig {
+    struct IAPPaymentProduct: Equatable {
+        public var productName: String?
+        public var productPrice: Double?
+        public var productPriceDivide: Double?
+        public var productTimeLabel: String?
+        public var productLocale: String?
+        public var productLegalText: String?
+        public var productButtonTitle: String?
+        public var productTrailBadge: Bool?
+
+        public var hasTrial: Bool {
+            productTrailBadge == true
+        }
+
+        public var hasSubtitle: Bool {
+            !(productPriceDivide == nil || productPriceDivide == 0)
+        }
+    }
+}

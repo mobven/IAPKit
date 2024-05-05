387a85d4f64fdeea605ed52824dc20e1cf18f80d
Eser Kucuker <eser.kucuker@mobven.com>
2024-05-05 12:35:52 +0300

    make  IAPProducts  equatable

--- Sources/IAPKit/IAPProduct/IAPProducts.swift
+++ Sources/IAPKit/IAPProduct/IAPProducts.swift
@@ -11,6 +11,7 @@ public struct IAPProducts {
     public let products: [IAPProduct]
     public let config: [String: Any]?
     public let paywallId: String?
+    public let productConfigs: IAPPaymentConfig?
 
     init(
         products: [IAPProduct],
@@ -20,5 +21,22 @@ public struct IAPProducts {
         self.products = products
         self.config = config
         self.paywallId = paywallId
+        self.productConfigs = .init(withParams: config ?? [:])
+    }
+}
+
+extension IAPProducts: Equatable {
+    public static func == (lhs: IAPProducts, rhs: IAPProducts) -> Bool {
+        guard lhs.products == rhs.products else {
+            return false
+        }
+        if let lhsConfig = lhs.config, let rhsConfig = rhs.config {
+            guard NSDictionary(dictionary: lhsConfig).isEqual(to: rhsConfig) else {
+                return false
+            }
+        } else if lhs.config != nil || rhs.config != nil {
+            return false
+        }
+        return lhs.paywallId == rhs.paywallId
     }
 }

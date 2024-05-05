07416e64ec06fec710c461274114d02da68094d4
Eser Kucuker <eser.kucuker@mobven.com>
2024-05-05 12:34:45 +0300

    add IAPPaymentProductExtension

---
+++ Sources/IAPKit/IAPPaymentConfig/IAPPaymentProductExtension.swift
@@ -0,0 +1,138 @@
+//
+//  IAPPaymentProductExtension.swift
+//
+//
+//  Created by Eser Kucuker on 5.05.2024.
+//
+
+import Foundation
+import StoreKit
+
+public extension IAPPaymentConfig.IAPPaymentProduct {
+
+    static func == (lhs: IAPPaymentConfig.IAPPaymentProduct, rhs: IAPPaymentConfig.IAPPaymentProduct) -> Bool {
+        lhs.productName == rhs.productName &&
+        lhs.productPrice == rhs.productPrice &&
+        lhs.productPriceDivide == rhs.productPriceDivide &&
+        lhs.productTimeLabel == rhs.productTimeLabel &&
+        lhs.productLocale == rhs.productLocale &&
+        lhs.productLegalText == rhs.productLegalText &&
+        lhs.productButtonTitle == rhs.productButtonTitle &&
+        lhs.productTrailBadge == rhs.productTrailBadge
+    }
+
+    /// Returns the subtitle for a given product, considering its subscription price, localized time label, and default time value.
+    /// - Parameters:
+    ///   - skProduct: The `IAPProduct` representing the product
+    ///   - productTimeLocalized: The localized time label for the product. It must be localized string,
+    ///   - defaultProductTime: The default time value to be used if `productTimeLocalized` is not available..It must be localized string,
+    /// - Returns:  A string representing the formatted subtitle for the product,
+    /// including its price and time information.
+    /// Returns `nil` if `productPriceDivide` is `nil` or less than or equal to zero.
+    /// Example usage:
+    /// ```
+    /// let product = IAPProduct(...)
+    /// let localizedSubtitle = subtitleForProduct(product, productTimeLocalized: "Month"localized(), defaultProductTime: "week".localized())
+    /// print(localizedSubtitle) // Output: "$4.99/Month"
+    /// ```
+    public func subtitleForProduct(
+        _ skProduct: IAPProduct?,
+        productTimeLocalized: String?,
+        defaultProductTime: String?
+    ) -> String? {
+        let productPriceDivide = productPriceDivide
+        let productPrice1 = skProduct?.subsciptionPrice.doubleValue ?? 0.0
+        let productTimeLabel = productTimeLocalized.isNilOrEmpty ? defaultProductTime : productTimeLocalized
+
+        let currencySymbol = skProduct?.priceLocale.currencySymbol ?? ""
+        let floatValue = productPrice1
+
+        guard productPriceDivide != nil && productPriceDivide ?? 0 > 0 else { return nil }
+
+        let truncatedValue = floor(floatValue / (productPriceDivide ?? 0) * 100) / 100
+
+        let formattedPrice = String(format: "%.2f", truncatedValue)
+            .replacingOccurrences(of: ".", with: ",")
+            .replacingOccurrences(of: "\\,?0+$", with: "", options: .regularExpression)
+
+        let timeString = (productTimeLabel != nil ? productTimeLabel : defaultProductTime) ?? ""
+
+        return "\(currencySymbol)\(formattedPrice)" + "/" + timeString
+    }
+
+    /// Returns the name for a given product You should use the returned string with the localized function. You should use the returned string with the localized function.
+    /// - Parameters:
+    ///   - skProduct: The `IAPProduct` representing the product
+    ///   - defaultlocalizedDateText: The default localized date text to be used if `productName` is not available.  It must be localized string,
+    /// - Returns:  A string representing the name of the product. If `productName` is nil or empty, returns `defaultlocalizedDateText`.
+    /// Example usage:
+    /// ```
+    /// let product = IAPProduct(...)
+    /// var config: IAPPaymentConfig.IAPPaymentProduct?
+    /// let productName = "productTitle:\(product.identifier)".localized
+    /// let localizedSubtitle = config?.name(forSKProduct: product, defaultlocalizedDateText: productName)?.localized
+    /// print(localizedSubtitle) // Output: ""3 Days Free""
+    /// ```
+    public func name(forSKProduct skProduct: IAPProduct?, defaultlocalizedDateText: String?) -> String? {
+        productName.isNilOrEmpty ? defaultlocalizedDateText : productName
+    }
+
+    /// Returns the price title for a given product and product locale.
+    /// - Parameters:
+    ///   - skProduct: The `IAPProduct` representing the product
+    ///   - productLocale: The locale of the product.  It must be localized string,
+    /// - Returns:A string representing the formatted price title of the product including its price and locale.
+    /// Example usage:
+    /// ```
+    /// let product = IAPProduct(...)
+    /// var config: IAPPaymentConfig.IAPPaymentProduct?
+    /// let defaultUnitName = (config?.productLocale == nil ) ? product.subscriptionPeriodText.localized : (config?.productLocale?.localized ?? "")
+    /// let productPrice = config?.priceTitle(skProduct: product, productLocale: defaultUnitName)
+    /// print(productPrice) // Output: "$4.99/USD" (if available) or "0.99/USD" (if not available)
+    /// ```
+    public func priceTitle(skProduct: IAPProduct?, productLocale: String) -> String {
+
+        let price2 = productPrice
+        let divide = price2.isNil || price2 ?? 0 > 0 ? (price2 ?? 0) : 1.0
+
+        var customPrice = skProduct?.subsciptionPrice.doubleValue ?? 0.0
+
+        customPrice = floor(customPrice / divide * 100) / 100
+
+        let formattedPrice = formattedCustomPrice(customPrice, alternativePrice: skProduct?.priceString() ?? "")
+
+        let skCurrency = skProduct?.priceLocale.currencySymbol ?? ""
+        let price = (customPrice > 0) ? skCurrency + formattedPrice : formattedPrice
+        return price + "/" + productLocale
+    }
+
+    public func formattedCustomPrice(_ customPrice: Double, alternativePrice: String) -> String {
+        (customPrice > 0) ? formatedCustomPrice(customPrice) : alternativePrice
+    }
+
+    public func formatedCustomPrice(_ customPrice: Double) -> String {
+        if customPrice.truncatingRemainder(dividingBy: 1.0) == .zero {
+            String(format: "%.0f", customPrice).replacingOccurrences(of: ".", with: ",")
+        } else {
+            String(format: "%.2f", customPrice).replacingOccurrences(of: ".", with: ",")
+        }
+    }
+
+    /// Returns the button title for a given product. You should use the returned string with the localized function
+    /// - Parameters:
+    ///   - skProduct: The `IAPProduct` representing the product
+    ///   - defaultButtonTitle: The default button title to be used if `productButtonTitle` is not available.  It must be localized string,
+    /// - Returns: A string representing the button title of the product. Returns `defaultButtonTitle` if `productButtonTitle` is nil or empty.
+    /// You should use the returned string with the localized function
+    /// Example usage:
+    /// ```
+    /// let product = IAPProduct(...)
+    /// var config: IAPPaymentConfig.IAPPaymentProduct?
+    /// let defaultButtonTitle = "Subscribe"
+    /// let buttonTitle = buttonTitle(skProduct: product, defaultButtonTitle: defaultButtonTitle)
+    /// print(buttonTitle) // Output: "Subscribe" (if available) or "Become Premium" (if not available)
+    /// ```
+    public func buttonTitle(skProduct: IAPProduct?, defaultButtonTitle: String) -> String? {
+        productButtonTitle.isNilOrEmpty ? defaultButtonTitle : productButtonTitle
+    }
+}

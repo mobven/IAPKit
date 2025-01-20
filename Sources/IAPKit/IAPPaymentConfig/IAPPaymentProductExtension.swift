d303be6a3ad66d421ee4e7ee6eed1322d5c8992e
Hakan <hakan.kumdakci@mobven.com>
2025-01-20 22:29:42 +0300

    added formattedPriceTitle to get various prices with multiplier

--- Sources/IAPKit/IAPPaymentConfig/IAPPaymentProductExtension.swift
+++ Sources/IAPKit/IAPPaymentConfig/IAPPaymentProductExtension.swift
@@ -110,6 +110,51 @@ public extension IAPPaymentConfig.IAPPaymentProduct {
         let price = (customPrice > 0) ? skCurrency + formattedPrice : formattedPrice
         return price + "/" + productLocale
     }
+    
+    func formattedPriceTitle(
+        skProduct: IAPProduct?,
+        productLocale: String,
+        multiplier: Double = 1.0
+    ) -> String {
+        // Get the base product price or set a default value
+        let basePrice = productPrice
+        let divisor = (basePrice.isNil || (basePrice ?? 0) <= 0) ? 1.0 : (basePrice ?? 1.0)
+
+        // Calculate the custom price based on the subscription price
+        var customPrice = Decimal(skProduct?.subsciptionPrice.doubleValue ?? 0.0)
+        let divisorDecimal = Decimal(divisor)
+        let scaledPrice = customPrice / divisorDecimal * Decimal(100)
+
+        // Round the scaled price to two decimal places
+        customPrice = (
+            (scaledPrice as NSDecimalNumber)
+                .rounding(accordingToBehavior: nil) as Decimal
+        ) / Decimal(100)
+
+        // Convert to Double for truncation and further operations
+        var truncatedPrice = NSDecimalNumber(decimal: customPrice).doubleValue
+
+        // Apply multiplier logic
+        if multiplier != 1.0 {
+            truncatedPrice *= multiplier
+            truncatedPrice = floor(truncatedPrice) + 0.99 // Apply pricing convention
+        }
+
+        // Format the final price
+        let formattedPrice = formattedCustomPrice(
+            truncatedPrice,
+            alternativePrice: skProduct?.priceString() ?? ""
+        )
+
+        // Get the currency symbol
+        let currencySymbol = skProduct?.priceLocale.currencySymbol ?? ""
+        
+        // Prepend the currency symbol only if the price is greater than 0
+        let priceWithCurrency = (truncatedPrice > 0) ? currencySymbol + formattedPrice : formattedPrice
+
+        // Return the final price string with locale
+        return priceWithCurrency + "/" + productLocale
+    }
 
     func formattedCustomPrice(_ customPrice: Double, alternativePrice: String) -> String {
         (customPrice > 0) ? formatedCustomPrice(customPrice) : alternativePrice

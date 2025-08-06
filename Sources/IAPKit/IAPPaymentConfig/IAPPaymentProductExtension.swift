595487ab306f4cf7ae9973371f9e36f23734e330
cansuozdizlekli <cansu.ozdizlekli@mobven.com>
2025-08-06 19:41:04 +0300

    format the price to show only two digits

--- Sources/IAPKit/IAPPaymentConfig/IAPPaymentProductExtension.swift
+++ Sources/IAPKit/IAPPaymentConfig/IAPPaymentProductExtension.swift
@@ -99,11 +99,16 @@ public extension IAPPaymentConfig.IAPPaymentProduct {
 
         let divideDecimal = Decimal(divide) // Ensure divide is also a Decimal
         
-        customPrice = customPrice / divideDecimal
-
-        let doubleCustomPrice = NSDecimalNumber(decimal: customPrice).doubleValue
-
-        let formattedPrice = formattedCustomPrice(doubleCustomPrice, alternativePrice: skProduct?.priceString() ?? "")
+        let decimalValue: Decimal = customPrice / divideDecimal
+        let number = decimalValue as NSDecimalNumber
+        var text = number.stringValue
+        let formattedPrice: String
+        
+        if text.contains(".00") {
+            formattedPrice = String(format: "%.0f", text).replacingOccurrences(of: ".", with: ",")
+        } else {
+            formattedPrice = String(format: "%.2f", text).replacingOccurrences(of: ".", with: ",")
+        }
 
         let skCurrency = skProduct?.priceLocale.currencySymbol ?? ""
         let price = (customPrice > 0) ? skCurrency + formattedPrice : formattedPrice

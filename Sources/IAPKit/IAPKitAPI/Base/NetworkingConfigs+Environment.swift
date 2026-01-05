c441f3c681449b7e98981bf984b70db2d7ccae04
Hakan <hakan.kumdakci@mobven.com>
2026-01-05 09:52:59 +0200

    added /

--- Sources/IAPKit/IAPKitAPI/Base/NetworkingConfigs+Environment.swift
+++ Sources/IAPKit/IAPKitAPI/Base/NetworkingConfigs+Environment.swift
@@ -20,8 +20,8 @@ public extension NetworkingConfigs {
 
         var baseURL: String {
             switch self {
-            case .development: "https://ioslab.online"
-            case .production: "http://ioslab.online"
+            case .development: "https://ioslab.online/"
+            case .production: "http://ioslab.online/"
             }
         }
     }
